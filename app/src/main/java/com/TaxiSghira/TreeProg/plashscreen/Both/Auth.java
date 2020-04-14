05df2007392162e6264d207e82a322064c27da6e
ayoub ghoudan <ayoubghoudanos@gmail.com>
2020-04-14 21:17:54 +0100

    4/14/2020 : 21:17

--- app/src/main/java/com/TaxiSghira/TreeProg/plashscreen/Both/Auth.java
+++ app/src/main/java/com/TaxiSghira/TreeProg/plashscreen/Both/Auth.java
@@ -17,18 +17,47 @@ import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.jakewharton.rxbinding3.view.RxView;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import kotlin.Unit;
+import timber.log.Timber;
+
 
 public class Auth extends AppCompatActivity {
 
     private GoogleSignInClient mGoogleSignInClient;
     private int Sign_In_Request_Code = 9001;
-
+    CompositeDisposable compositeDisposable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
-        findViewById(R.id.buttonphone).setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), phoneNumber.class)));
-        findViewById(R.id.TxtGoogle).setOnClickListener(v -> signIn());
+
+        compositeDisposable = new CompositeDisposable();
+        RxView.clicks(findViewById(R.id.buttonphone))
+                .throttleFirst(5, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Unit>() {
+                    @Override
+                    public void onSubscribe(Disposable d) { compositeDisposable.add(d); }
+
+                    @Override
+                    public void onNext(Unit unit) { signIn(); }
+
+                    @Override
+                    public void onError(Throwable e) { Timber.e(e); }
+
+                    @Override
+                    public void onComplete() { Timber.e("Map Activity : Called");}
+                });
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
@@ -66,5 +95,11 @@ public class Auth extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), Map.class));
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        compositeDisposable.clear();
     }
 }
