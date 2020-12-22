312fbbf7a4a4423cf17053286be61c6bbd453450
ayoub <ayoubghoudanos@gmail.com>
2020-12-22 18:12:52 +0100

    Complete Trip Notification

---
+++ app/src/main/java/com/TaxiSghira/TreeProg/plashscreen/Module/EventBus/DriverCompleteTrip.java
@@ -0,0 +1,15 @@
+package com.TaxiSghira.TreeProg.plashscreen.Module.EventBus;
+
+public class DriverCompleteTrip {
+    private String key;
+    public DriverCompleteTrip() {
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
